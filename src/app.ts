08a30a50f80c4be605fcb255dcb2027c9f00acc2
yubinkarki <iamyubinkarki@gmail.com>
2026-03-02 21:03:29 +0900

    Listening of the express app hell ya

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-const appPort: number = process.env.APP_PORT;
+import { default as expressApp } from "#config/express.config";
 
-console.log(appPort);
+import { appStrings } from "#constant/common-string.constant";
+import { errorMessage as EM } from "#constant/http.constant";
+
+const appPort: string | undefined = process.env.APP_PORT;
+const appEnv: string | undefined = process.env.APP_ENVIRONMENT;
+
+if (!appPort || !appEnv) throw new Error(EM.missingRequiredEnv);
+
+expressApp.set("trust proxy", true);
+
+expressApp
+  .listen(appPort, () => console.info(`${appStrings.startSuccess} at port ${appPort}`))
+  .on("error", (e: unknown) => console.error("error starting the app", e));
